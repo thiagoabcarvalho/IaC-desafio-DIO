a1e9a86d5ecfa01615cfa95896dec82df6e371f2
Thiago Carvalho <thiagoabcarvalho@gmail.com>
2022-08-28 22:26:47 -0300

    Apache v.1.o

---
+++ script-iac2.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+echo "Atualizando o servidor..."
+apt-get update
+apt-get upgrade -y
+apt-get install apache2 -y
+apt-get install unzip -y
+
+
+echo "Baixando e copiando os arquivos da aplicação..."
+
+
+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
+cd /tmp
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio-main
+cp -R * /var/www/html/
+
+
+
+
